36459cb06f0aaee61d976359967a654feb2e3fb0
Lucas <lucas.sv95@hotmail.com>
2023-02-17 11:23:46 -0300

    feat: create component Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { Toaster } from "react-hot-toast";
 import "./App.css";
-import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <Toaster />
-      <Header  />    
+      <Toaster /> 
     </>
   )
 }
